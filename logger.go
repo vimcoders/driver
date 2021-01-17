60e93d0d8a65ac1361cd04c7d1e1210ffe5a3f44
coder <coder@57blocks.local>
2021-01-17 10:16:22 +0800

    logger

---
+++ logger.go
@@ -0,0 +1,8 @@
+package driver
+
+type Logger interface {
+	Debug(format string, v ...interface{})
+	Info(format string, v ...interface{})
+	Warning(format string, v ...interface{})
+	Error(format string, v ...interface{})
+}
