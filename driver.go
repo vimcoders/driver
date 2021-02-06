4638c33b8e8648f4c3c194ca7fcc89cbd3bda0ef
57blocks <57blocks@57blocks.local>
2021-02-06 21:27:15 +0800

    logger

--- driver.go
+++ driver.go
@@ -5,6 +5,7 @@ type Logger interface {
 	Info(format string, v ...interface{})
 	Warning(format string, v ...interface{})
 	Error(format string, v ...interface{})
+	Close() (err error)
 }
 
 type Messager interface {
